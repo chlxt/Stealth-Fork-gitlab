6c8a926812e4215294d4f67ed1f73afb60b1a69c
CosmosDev <cosmos.dev@protonmail.com>
2023-10-20 22:30:31 +0200

    Bump dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -10,7 +10,7 @@ object Dependencies {
         const val core = "1.12.0"
         const val appCompat = "1.6.1"
         const val constraintLayout = "2.1.4"
-        const val recyclerview = "1.3.1"
+        const val recyclerview = "1.3.2"
         const val fragment = "1.6.1"
         const val lifecycle = "2.6.2"
         const val coordinatorlayout = "1.2.0"
@@ -19,14 +19,14 @@ object Dependencies {
 
         const val navigation = "2.5.3"
 
-        const val room = "2.6.0-rc01"
+        const val room = "2.6.0"
         const val datastore = "1.0.0"
 
         const val paging = "3.2.1"
 
         const val work = "2.8.1"
  
-        const val material = "1.9.0"
+        const val material = "1.10.0"
  
         const val browser = "1.6.0"
  
