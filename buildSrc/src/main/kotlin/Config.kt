090f295afb46350c905ec419ebd84d57c02bb564
CosmosDev <cosmos.dev@protonmail.com>
2023-10-21 19:36:01 +0200

    Bump version code and name

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -7,6 +7,6 @@ object Config {
     const val compileSdk = 34
     const val targetSdk = 34
 
-    const val versionCode = 22
-    const val versionName = "2.3.0"
+    const val versionCode = 23
+    const val versionName = "3.0.0-alpha01"
 }
