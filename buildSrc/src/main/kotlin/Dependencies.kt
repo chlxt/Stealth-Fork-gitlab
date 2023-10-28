d29f235df557fe80056ccc738007e3a97a62e62a
CosmosDev <cosmos.dev@protonmail.com>
2023-10-28 16:52:02 +0200

    Bump Stealth SDK to 0.1.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -52,7 +52,7 @@ object Dependencies {
 
         const val jsonSerialization = "1.5.1"
 
-        const val stealth = "0.1.0"
+        const val stealth = "0.1.1"
 
         const val jUnit = "4.13.2"
         const val test = "1.1.5"
